2e1d688208415958e45ee95fa64be4ba14b1016d
Matin <95539953+matindev0@users.noreply.github.com>
2022-01-08 10:39:14 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    token: "OTI4NjA5NzgwOTY3MzAxMTMx.YdbRQA.vV7jHieQttfZElHspoeQIy0fYko",
-    prefix: "."
+    token: "OTI4NjA5NzgwOTY3MzAxMTMx.YdbRQA.T8dW8RNe7c8tPqju6ZCxOjV9bt0",
+    prefix: "--"
 }
+
+//
